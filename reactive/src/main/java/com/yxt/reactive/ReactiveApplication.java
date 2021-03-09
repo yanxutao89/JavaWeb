8d8e60ac12db4287b462a5eddeb6411eb6428743
yanxt7 <yanxt7@chinaunicom.cn>
2021-03-09 13:20:31 +0800

    20210309

--- reactive/src/main/java/com/yxt/reactive/ReactiveApplication.java
+++ reactive/src/main/java/com/yxt/reactive/ReactiveApplication.java
@@ -1,6 +1,5 @@
 package com.yxt.reactive;
 
-import com.yxt.reactive.client.GreetingWebClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,12 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date: 2021/03/04 13:38
  */
 @SpringBootApplication
-public class Application {
+public class ReactiveApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-
-		GreetingWebClient gwc = new GreetingWebClient();
-		System.out.println(gwc.getResult());
+		SpringApplication.run(ReactiveApplication.class, args);
 	}
 }
