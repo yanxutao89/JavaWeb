ce081928fbd8d04f2b21b080ece6d7b386d86d34
yanxt7 <yanxt7@chinaunicom.cn>
2021-05-23 14:38:13 +0800

    20210523

---
+++ netty/src/main/java/ch11/HttpRequestHandler.java
@@ -0,0 +1,65 @@
+package ch11;
+
+import io.netty.channel.*;
+import io.netty.handler.codec.http.*;
+import io.netty.handler.ssl.SslHandler;
+import io.netty.handler.stream.ChunkedNioFile;
+
+import java.io.RandomAccessFile;
+
+public class HttpRequestHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
+
+    private final String wsUri;
+
+    public HttpRequestHandler(String wsUri) {
+        this.wsUri = wsUri;
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, FullHttpRequest request) throws Exception {
+        if (wsUri.equalsIgnoreCase(request.getUri())) {
+            ctx.fireChannelRead(request.retain());
+        }
+        else {
+            if (HttpHeaders.is100ContinueExpected(request)) {
+                send100Continue(ctx);
+            }
+
+            RandomAccessFile file = new RandomAccessFile("netty/src/main/resources/index.html", "r");
+
+            DefaultHttpResponse response = new DefaultHttpResponse(request.getProtocolVersion(), HttpResponseStatus.OK);
+            response.headers().set(HttpHeaders.Names.CONTENT_TYPE, "text/plain; charset=UTF-8");
+
+            boolean keepAlive = HttpHeaders.isKeepAlive(request);
+
+            if (keepAlive) {
+                response.headers().set(HttpHeaders.Names.CONTENT_LENGTH, file.length());
+                response.headers().set(HttpHeaders.Names.CONNECTION, HttpHeaders.Values.KEEP_ALIVE);
+            }
+            ctx.write(response);
+
+            if (ctx.pipeline().get(SslHandler.class) == null) {
+                ctx.write(new DefaultFileRegion(file.getChannel(), 0, file.length()));
+            }
+            else {
+                ctx.write(new ChunkedNioFile(file.getChannel()));
+            }
+
+            ChannelFuture future = ctx.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT);
+            if (!keepAlive) {
+                future.addListener(ChannelFutureListener.CLOSE);
+            }
+        }
+    }
+
+    private static void send100Continue(ChannelHandlerContext ctx) {
+        DefaultHttpResponse response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.CONTINUE);
+        ctx.writeAndFlush(response);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
