666d82fd98d2cf696af6762ac25d9a0f250e754d
yanxt7 <yanxt7@chinaunicom.cn>
2021-05-24 15:30:02 +0800

    202105242

--- netty/src/main/java/ch12/SecureChatServer.java
+++ netty/src/main/java/ch12/SecureChatServer.java
@@ -1,19 +1,24 @@
-package ch11;
+package ch12;
 
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.group.ChannelGroup;
-import utils.BogusSslContextFactory;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
 
-import javax.net.ssl.SSLContext;
 import java.net.InetSocketAddress;
 
+/**
+ * Listing 12.7 Adding encryption to the ChatServer
+ *
+ * @author <a href="mailto:norman.maurer@gmail.com">Norman Maurer</a>
+ */
 public class SecureChatServer extends ChatServer {
 
-    private final SSLContext context;
+    private final SslContext context;
 
-    public SecureChatServer(SSLContext context) {
+    public SecureChatServer(SslContext context) {
         this.context = context;
     }
 
@@ -22,19 +27,17 @@ public class SecureChatServer extends ChatServer {
         return new SecureChatServerInitializer(group, context);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         if (args.length != 1) {
             System.err.println("Please give port as argument");
             System.exit(1);
         }
         int port = Integer.parseInt(args[0]);
-
-        SSLContext context = BogusSslContextFactory.getServerContext();
+        SelfSignedCertificate cert = new SelfSignedCertificate();
+        SslContext context = SslContext.newServerContext(cert.certificate(), cert.privateKey());
         final SecureChatServer endpoint = new SecureChatServer(context);
         ChannelFuture future = endpoint.start(new InetSocketAddress(port));
-
         Runtime.getRuntime().addShutdownHook(new Thread(() -> endpoint.destroy()));
         future.channel().closeFuture().syncUninterruptibly();
     }
-
 }
---
+++ netty/src/main/java/ch12/SpdyRequestHandler.java
@@ -0,0 +1,11 @@
+package ch12;
+
+
+public class SpdyRequestHandler extends HttpRequestHandler2 {
+
+    @Override
+    protected String getContent() {
+        return "This content is transmitted via SPDY\r\n";
+    }
+
+}
